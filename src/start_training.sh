eaec58bb44d509ac7b44c7942d42c98d7ef0f06d
Silva Freitas Neto, Arnold (TX) <arnold.silva.freitas.neto@iav.de>
2023-03-30 18:44:30 +0000

    memory trace

--- src/start_training.sh
+++ src/start_training.sh
@@ -2,5 +2,5 @@ echo $$
 echo $BASHPID
 export TRAINTIME=$(date +"%Y%d%d_%H%M%S")
 echo $TRAINTIME 
-python -m trace --trace train.py |& tee -a ../data/log_$TRAINTIME.txt
-# fil-profile train.py |& tee -a ../data/log_$TRAINTIME.txt
+# python train.py |& tee -a ../data/log_$TRAINTIME.txt
+fil-profile run train.py |& tee -a ../data/log_$TRAINTIME.txt
